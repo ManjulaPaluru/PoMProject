8ed2a40f0cc01fd1aefb668784159ef314226ea3
mpaluru <manjula.paluru@gmail.com>
2022-04-15 11:59:32 -0700

    adding package name to testig

--- src/main/java/com/qa/opencart/pages/Testing.java
+++ src/main/java/com/qa/opencart/pages/Testing.java
@@ -1,7 +1,8 @@
-public class Testing
-{
-  public static void main(String args[]){
+package com.qa.opencart.pages;
+
+public class Testing {
+  public static void main(String args[]) {
     System.out.println("this is sample test is added by developer 1");
   }
-    
+
 }
