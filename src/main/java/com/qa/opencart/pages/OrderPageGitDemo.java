a5326b071bf3f63a038f1fc6fbb8ab3ca9376baf
ManjulaPaluru <92763225+ManjulaPaluru@users.noreply.github.com>
2022-04-15 14:52:28 -0700

    Create OrderPageGitDemo.java

---
+++ src/main/java/com/qa/opencart/pages/OrderPageGitDemo.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class OrderPageGitDemo {
+    public static void makeOrder() {
+        System.out.println("orderpage");
+    }
+public static void getOrderId(){
+  System.out.println("order id....)}
+    public static void main(String[] args) {
+
+    }
+}
