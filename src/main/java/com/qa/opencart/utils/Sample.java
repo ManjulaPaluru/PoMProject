7479cb09ff6da035578e5eb2042be4465e4eabbd
ManjulaPaluru <92763225+ManjulaPaluru@users.noreply.github.com>
2022-04-15 14:27:24 -0700

    Create Sample.java

---
+++ src/main/java/com/qa/opencart/utils/Sample.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.utils;
+
+public class Sample {
+    public void login() {
+        String userName = "tom";
+        System.out.println(userName);
+    }
+
+    public static void main(String[] args) {
+    }
+}
